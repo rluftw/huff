91aaa4521de103f4e095d7076b04706440ef0809
rluftw <luxh93@gmail.com>
2017-09-17 16:39:50 -0500

    complete ranking table extraction to its own tab

--- huff/controllers/HomeViewController.swift
+++ huff/controllers/HomeViewController.swift
@@ -15,27 +15,12 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var quoteAuthor: UILabel!
     
-//    @IBOutlet weak var topDistanceTable: UITableView!
-    
-    // MARK: - properties
-    var topRecordHandle: DatabaseHandle?
-    var topRecords = [TopRunRecord]()
-    
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // check first time login and fetch global high score
-        configureHomescreen()
 
         // fetch the remote configurations
         fetchQuotes()
-    }
-    
-    
-    // MARK: - initialization
-    deinit {
-        FirebaseService.sharedInstance().removeObserver(handler: topRecordHandle)
     }
     
     // MARK: - actions
@@ -57,42 +42,4 @@ class HomeViewController: UIViewController {
         }
     }
     
-    func configureHomescreen() {
-        // check if this is the users first time logging in
-        FirebaseService.sharedInstance().fetchAccountNode { (localSnapshot) in
-            guard let snapshot = localSnapshot.value as? [String: Any], let _ = snapshot["creation_date"] as? TimeInterval else {
-                print("no creation date found - attempting to set a creation date")
-                FirebaseService.sharedInstance().setAccountCreationDate()
-                return
-            }
-        }
-        topRecordHandle = FirebaseService.sharedInstance().fetchGlobalHSDistance { (localSnapshot) in
-            self.handleTopRecords(localSnapshot: localSnapshot)
-        }
-    }
-    
-    func handleTopRecords(localSnapshot: DataSnapshot) {
-        guard let snapshot = localSnapshot.value as? [String: Any] else {
-            print("invalid snapshot format")
-            return
-        }
-        let record = TopRunRecord(dict: snapshot)
-        self.topRecords.insertOrdered(record)
-//        self.topDistanceTable.reloadData()
-    }
-}
-
-extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "topListCell", for: indexPath) as! TopListCell
-        
-        cell.rankLabel.text = "\(indexPath.row+1)."
-        cell.userLabel.text = indexPath.row <= topRecords.count-1 ? topRecords[indexPath.row].username : "--------------"
-        cell.valueLabel.text = indexPath.row <= topRecords.count-1 ? "\(String(format: "%.2f", topRecords[indexPath.row].distance/1609.34)) miles": "0.00 miles"
-        
-        return cell
-    }
 }
